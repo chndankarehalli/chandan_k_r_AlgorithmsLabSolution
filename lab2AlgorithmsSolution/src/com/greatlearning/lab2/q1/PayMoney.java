94e0964ed8dda91c7f89c10118a25c47aa627bf0
chandan <chandan.krc361991@gmail.com>
2021-09-19 11:12:22 +0530

    Initial Commit

---
+++ lab2AlgorithmsSolution/src/com/greatlearning/lab2/q1/PayMoney.java
@@ -0,0 +1,10 @@
+package com.greatlearning.lab2.q1;
+
+public class PayMoney {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lab2AlgorithmsSolution/src/com/greatlearning/lab2/q2/Traveler.java
@@ -0,0 +1,10 @@
+package com.greatlearning.lab2.q2;
+
+public class Traveler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
